cc82eeb9fe99193dd056b05a71a82ae0dc56be14
puram <vamsipuram844@gmail.com>
2025-07-20 21:32:33 +0530

    Update Files

--- replace_author.py
+++
@@ -1,8 +0,0 @@
-def commit_callback(commit):
-    if commit.author_name == b"eganagha" and commit.author_email == b"anagha147laksmi@gmail.com":
-        commit.author_name = b"Vamsi Prasad Puram"
-        commit.author_email = b"vamsipuram844@gmail.com"
-
-    if commit.committer_name == b"eganagha" and commit.committer_email == b"anagha147laksmi@gmail.com":
-        commit.committer_name = b"Vamsi Prasad Puram"
-        commit.committer_email = b"vamsipuram844@gmail.com"
